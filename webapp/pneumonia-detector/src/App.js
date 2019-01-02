fa9275267e3085794038efaf948d1a3f71f9adc3
LatrecheYasser <yasserlat@gmail.com>
2019-01-02 14:52:56 +0100

    creting something of the interface

--- webapp/pneumonia-detector/src/App.js
+++ webapp/pneumonia-detector/src/App.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Axios from 'axios';
+
+
 
 class App extends Component {
+  // this state is used to save the file the has been uploaded in this case it's the x-ray picture 
+  state={
+    selectedImage:null
+  }
+  
+
+  //this hendler is used to save the image into a state and turn set the uploadedImage variable to true so we can check after it we
+  //already have a pic saved , we can virifie if the selectedImage is null inside the state but i will work with this i will change it later
+
+  uploadedImage = false 
+  fileChangedHandler = (event) => {
+    this.setState({
+      selectedImage : event.target.files[0]
+    })
+    this.uploadedImage=true
+  }
+
+
+  // this is the evenhandler of the sumbmit clic , it sends the image using axios 
+  OnUploadHeanderl = () =>{
+    const fd =new FormData();
+    fd.append('chest_xray',this.state.selectedImage);
+    Axios.post('http://127.0.0.1:3003/pred',fd).then( response=>{
+      console.log('the respons is ',  response.data.json);
+    } ) 
+    console.log('nothing i guess')
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <h3>Pneumonia Detector</h3>
+          <div>
+          <img src="/Image/Pen.jpg" style={{float:'left'}} alt="an example of X-ray pic" ></img>
+          <h4 >Pneumonia detector</h4>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          a web site that helps Doctors and Normal people to detect the Pneumonia
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>
+          The detection of Pneumonia never been easy as today with the new Computer since and the new deep learning methods the detection of this sickens is so easy , we provide a website to help Doctors and other persons in this job
+          it's so easy all what you have to do is to load you x-ray image to our web site and the web site will show you how much is the probability to be sick or not
+          we really wish you a good  health .
+          </p>
+          
+            <input type="file" onChange={this.fileChangedHandler}/>
+            <button onClick={this.OnUploadHeanderl}>send</button>
+
+
+          
+          </div>
         </header>
       </div>
     );
