8a2b466ee07f4f0793c5428f1c9117478e0ff00d
youben11 <ayouben9@gmail.com>
2019-01-04 15:15:43 +0100

    polishing the ui

--- webapp/pneumonia-detector/src/App.js
+++ webapp/pneumonia-detector/src/App.js
@@ -22,7 +22,8 @@ class App extends Component {
     selectedImage:null,
     visible : false,
     Normal:0,
-    Sick:0
+    Sick:0,
+    waiting: false,
   }
 
   Results = {
@@ -42,44 +43,80 @@ class App extends Component {
     this.uploadedImage = true
   }
 
+  getSelectedImageName = () => {
+    if (this.uploadedImage){
+      return this.state.selectedImage.name
+    }else {
+      return ""
+    }
+  }
 
   // this is the evenhandler of the sumbmit clic , it sends the image using axios
 
   OnUploadHeanderl = () =>{
-    if(this.state.selectedImage==null)alert('You didn\'t choose any Picture please selecte one and click agin');
-    else{    const fd =new FormData();
-              fd.append('chest_xray',this.state.selectedImage);
-              // this url should be changed after for now the API is runing under this addres in my machine
-              Axios.post('http://130.211.108.207:3000/pred',fd).then( response=>{
-              this.setState({
-                Normal : response.data['NORMAL'],
-                Sick   : response.data['PNEUMONIA']
-              })
-              this.openModal()
-              })
-  }
+    if(this.state.selectedImage==null)
+      alert('You didn\'t choose any picture. Please select one and click agin');
+    else{
+      const fd =new FormData();
+      fd.append('chest_xray',this.state.selectedImage);
+      // this url should be changed after for now the API is runing under this addres in my machine
+      this.setState({
+        waiting: true
+      })
+      Axios.post('http://130.211.108.207:3000/pred',fd)
+        .then( response=>{
+          this.setState({
+              Normal : response.data['NORMAL'],
+              Sick   : response.data['PNEUMONIA']
+            })
+            this.setState({
+              waiting: false
+            })
+            this.openModal()
+        })
+        .catch(error => {
+          this.setState({
+            waiting: false
+          })
+          alert("Something went wrong :/ \nPlease raise an issue on our github repo")
+        })
+    }
 
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        <h3>Pneumonia Detector</h3>
+          <header className="App-header">
+          <div style={{margin: '20px'}}>
+          <a target="_blank" href="https://github.com/LatrecheYasser/pneumonia-detector">
+            <b>Pneumonia Detector</b>
+            <img src="/Image/github.png" style={{marginLeft:'10px', width:'4%'}}/>
+            </a>
+          </div>
           <div style={{backgroundColor:'#282c34'}}>
-          <img src="/Image/Pen.jpg" style={{float:'left'}} alt="an example of X-ray pic" ></img>
-          <h4 >Pneumonia detector</h4>
-          <p>
-          a web site that helps Doctors and Normal people to detect the Pneumonia
-          </p>
-          <p>
-          The detection of Pneumonia never been easy as today with the new Computer since and the new deep learning methods the detection of this sickens is so easy , we provide a website to help Doctors and other persons in this job
-          it's so easy all what you have to do is to load you x-ray image to our web site and the web site will show you how much is the probability to be sick or not
-          we really wish you a good  health .
-          </p>
+          <img src="/Image/Pen.jpg" style={{float:'left', margin:'20px'}} alt="an example of X-ray pic" ></img>
+          <h4>
+          We help you detect Pneumonia !
+          </h4>
+          <div id="desc">
+            <p>
+            The detection of Pneumonia has never been as easy as today with Deep Learning methods.
+            </p>
+            <p>
+            Our service helps doctors and other persons to diagnose Pneumonia as quickly as possible.
+            All what you have to do is to load your chest X-ray image, then we will show you the results in just few seconds.
+            </p>
+            <p>
+            We really wish you a good  health.
+            </p>
+          </div>
             <label className="button1"> Upload your X-ray
             <input type="file" onChange={this.fileChangedHandler}/></label>
-            <br/>
-            <button className="button1" onClick={this.OnUploadHeanderl}>See the Results</button>
+            <p visible={this.uploadedImage.toString()}>{this.getSelectedImageName()}</p>
+            <button className="button1" onClick={this.OnUploadHeanderl}>
+              {this.state.waiting? "Waiting..." : "See the Results"}
+            </button>
 
                 <Modal visible={this.state.visible} width="400" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}>
                     <div style={{color:'black'}}>
